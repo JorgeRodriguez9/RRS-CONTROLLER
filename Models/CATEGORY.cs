2784e4ccecbfca5eba8905c1b07555e97d70f357
carlosandresramos <carlosandresramos803@gmail.com>
2023-04-29 00:41:02 -0500

    Add Id

--- Models/CATEGORY.cs
+++ Models/CATEGORY.cs
@@ -1,6 +1,12 @@
-﻿namespace RRS_Controller.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RRS_Controller.Models
 {
     public class CATEGORY
     {
+
+        [Key]
+        public int ID { get; set; }
+
     }
 }
