6eeb786afcf1f1080c93442863cffb8ee402e724
carlosandresramos <84865039+carlosandresramos@users.noreply.github.com>
2023-05-04 14:27:34 -0500

    Add files via upload

---
+++ Models/ROLE.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RRS_Controller.Models
+{
+    public class ROLE
+    {
+
+        [Key]
+        public int ID { get; set; }
+
+        [StringLength(30)]
+        public string Name_Role { get; set; }
+
+        /////////////////
+        public virtual ICollection<USER> USERS { get; set; }
+
+
+    }
+}
