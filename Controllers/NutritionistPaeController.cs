cc2643fd8ca34d73e7943918c225c9950d75ec41
JorgeRodriguez9 <131720274+JorgeRodriguez9@users.noreply.github.com>
2023-05-04 14:46:46 -0500

    siNoElimino

---
+++ Controllers/NutritionistPaeController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RRS_Controller.Controllers
+{
+    public class NutritionistPaeController : Controller
+    {
+        // GET: NutritionistPaeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: NutritionistPaeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: NutritionistPaeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NutritionistPaeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NutritionistPaeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: NutritionistPaeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NutritionistPaeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: NutritionistPaeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
